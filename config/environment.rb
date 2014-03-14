cff4edb09a29c4fe03c3b7140afb24290fd4f4d7
Daniel Fenjves <fenjves.daniel@gmail.com>
2014-03-14 13:44:32 -0400

    postgres database

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,14 @@ require_relative '../models/check_if_open'
 require_relative '../controllers/application_controller'
 require_relative '../controllers/museums_controller'
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => 'db/development.sqlite3'
-)
+
+# ActiveRecord::Base.establish_connection(
+#   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+#   :host     => db.host,
+#   :username => db.user,
+#   :password => db.password,
+#   :database => db.path[1..-1],
+#   :encoding => 'utf8'
+# )
+
+ActiveRecord::Base.establish_connection('postgres://localhost/isitopen')
